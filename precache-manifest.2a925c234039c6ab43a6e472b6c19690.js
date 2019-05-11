aa6c1833bf26a1c659ffe1a40f381538553feb3f
WuWooLay <mamamglay.lp@gmail.com>
2019-05-11 20:05:49 +0630

    first commit

---
+++ precache-manifest.2a925c234039c6ab43a6e472b6c19690.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611d6a9771ef6748020d4fd84e8bfd43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7f7aa046aa37ee4010b",
+    "url": "/static/css/main.6a134c92.chunk.css"
+  },
+  {
+    "revision": "29f184eca52a68e53b39",
+    "url": "/static/js/2.cc65b3e4.chunk.js"
+  },
+  {
+    "revision": "b7f7aa046aa37ee4010b",
+    "url": "/static/js/main.6f2439f6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
